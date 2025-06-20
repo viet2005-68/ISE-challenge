57e52fa2a10e280b87e1e630bff6f0ec4f033519
thanhvinh0702 <thanhvinhhero0702@gmail.com>
2025-06-21 02:39:30 +0700

    Rm pycache

--- src/agents/coding_agent.py
+++ src/agents/coding_agent.py
@@ -1,10 +1,14 @@
 import os
 from ..state.agent_state import AgentState
 from langchain_openai import ChatOpenAI
+from constants.constants import BASE_URL, LLAMA_MODEL
 
 llm_coding = ChatOpenAI(
-    api_key=os.getenv("OPENAI_API_KEY"),
-    model=os.getenv("MODEL_NAME"),
+    # api_key=os.getenv("OPENAI_API_KEY"),
+    # model=os.getenv("MODEL_NAME"),
+    base_url=BASE_URL,
+    model='llama-3.3-70b-versatile',
+    api_key=os.getenv("GROQ_API_KEY"),
     temperature=0.5
 )
 
